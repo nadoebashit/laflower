5e1f73e20a4c09f2177ec802434f1185d68257fe
nadoebashit <114692432+nadoebashit@users.noreply.github.com>
2026-03-16 10:09:33 +0500

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
